ca27517eddb72f8b87af58e7181352200bf003d9
Michal <michal.fpv.2020@gmail.com>
2024-02-12 14:46:30 +0100

    V3.3

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mwikiera <mwikiera@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/12 14:32:14 by mwikiera          #+#    #+#             */
-/*   Updated: 2024/02/12 14:32:14 by mwikiera         ###   ########.ch       */
+/*   Created: 2024/02/12 14:46:21 by mwikiera          #+#    #+#             */
+/*   Updated: 2024/02/12 14:46:21 by mwikiera         ###   ########.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -139,7 +139,7 @@ int	ft_printf(const char *sign, ...)
 
 
 
-int main()
+/*int main()
 {
 	printf("My Tests:\n");
 	//printf("\nNumber of chars: %d\n", ft_printf("%s%s%s", "And ", "some", "joined"));
@@ -147,7 +147,7 @@ int main()
 	printf("\n-------------\n\nShould get:\n");
 	//printf("\nNumber of chars: %d\n", printf("%s%s%s", "And ", "some", "joined"));
 	printf("\nNumber of chars: %d\n", printf("%ss%ss%ss", "And ", "some other", "joined"));
-}
+}*/
 
 // gcc -g ft_printf.c ft_printf_utils.c -Ilibft -Llibft -lft
 // !!!! Make clean and Make every time
