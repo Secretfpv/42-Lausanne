ad4625c72a4390432f652d9bf3d78da996a32495
Michal <michal.fpv.2020@gmail.com>
2024-02-15 13:18:58 +0100

    V5.3

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mwikiera <mwikiera@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 15:48:01 by mwikiera          #+#    #+#             */
-/*   Updated: 2024/02/15 13:09:02 by mwikiera         ###   ########.ch       */
+/*   Updated: 2024/02/15 13:18:42 by mwikiera         ###   ########.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -101,7 +101,7 @@ void	base(unsigned int num, char *base)
 int	replace(char c, va_list args, int len)
 {
 	if(c == 'd') {
-		len = len + decimal(va_arg(args, int), len);
+		len = decimal(va_arg(args, int), len);
 	}
 	else if (c == 's')
 	{
@@ -171,15 +171,15 @@ int	ft_printf(const char *sign, ...)
 
 
 
-/*int main()
+int main()
 {
 	printf("My Tests:\n");
 	//printf("\nNumber of chars: %d\n", ft_printf("%s%s%s", "And ", "some", "joined"));
-	printf("\nNumber of chars: %d\n", ft_printf("%d", 0));
+	printf("\nNumber of chars: %d\n", ft_printf("dgs%dxx", 10));
 	printf("\n-------------\n\nShould get:\n");
 	//printf("\nNumber of chars: %d\n", printf("%s%s%s", "And ", "some", "joined"));
-	printf("\nNumber of chars: %d\n", printf("%d", 0));
-}*/
+	printf("\nNumber of chars: %d\n", printf("dgs%dxx", 10));
+}
 
 // gcc -g ft_printf.c ft_printf_utils.c -Ilibft -Llibft -lft
 // !!!! Make clean and Make every time
